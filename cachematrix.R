921bdb64eaaa8a94c1befd23c9f10385b321275d
ari-git <ari.git@mail.com>
2015-02-20 20:42:14 -0500

    makeCacheMatrix and cacheSolve functions defined in cachematrix.R, with appropriate comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R is an implementation of 'caching' the inverse
+## of a matrix, and returning the 'inverse' from the 'cache'
+## when appropriate (if a 'cached' inverse exists for the matrix)
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## function 'makeCacheMatrix' defines the get and set functions
+## for the input matrix and its inverse 
+##
+## INPUT: a 'square' matrix (assumed 'invertible')
+## OUTPUT: list containing the 'get', 'set' functions
+##         for the original matrix and its inverse
+makeCacheMatrix <- function (matx = matrix()) {
+        
+        # initialize/purge variable for inverse
+        inv <- NULL
+        
+        # set, get functions for new matrix
+        # 'setMat': sets new matrix in global cache when invoked,
+        #           resets 'inverse' in global cache
+        # 'getMat': retrieves matrix (if already set) from global cache
+        setMat <- function (mat_new) {
+                matx <<- mat_new
+                inv <<- NULL
+        }
+        getMat <- function () matx
+        
+        # set, get functions for inverse matrix
+        # 'setInv': sets inverse (if not already set) in global cache
+        # 'getInv': retrieves inverse (if already set) from global cache
+        setInv <- function (matinv) inv <<- matinv
+        getInv <- function () inv
+        
+        # return list of the get, set function pairs for
+        # matrix & inverse matrix
+        list (setMat = setMat, getMat = getMat,
+              setInv = setInv, getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## function 'cacheSolve' returns the 'inverse' of the matrix
+## either by calculating afresh or by retrieving from the cache
+## -- the first time 'cacheSolve is explicitly called on a
+## -- (assumptions: square, invertible) matrix, the 'inverse'
+## -- will be computed and set in the global cache for that matrix
+## 
+## INPUT: a 'square' matrix (assumed 'invertible') and 'optional' fields
+## OUTPUT: 'inverse' of the square matrix
+cacheSolve <- function (matx, ...) {
+        # attempt to retrieve cached inverse for 'matx' and
+        # return 'matinv' (the inverse) if 'not' NULL ('inverse' exists)
+        matinv <- matx$getInv ()
+        if(!is.null (matinv)) {
+                message("getting cached data")
+                return (matinv)
+        }
+        
+        # if 'matinv' is NULL (has not yet been set in global cache), then
+        # - 1. get 'data' from 'matx' global cache
+        # - 2. compute inverse ('matinv') by invoking 'base::solve()'
+        # - 3. set the computed inverse into global cache for 'matx'
+        data <- matx$getMat()
+        matinv <- solve (data)
+        matx$setInv (matinv)
+        
+        # return a matrix that is the inverse of 'matx'
+        return (matinv)
 }
